2687eb4350886ecc1b06daa54915c75d5795953d
Leon Knauf <leon.knauf@gmail.com>
2023-01-04 19:40:27 +0100

    Theoretisch Fertig

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect
 from selbst_kontrolle_script import Quiz
 
 app = Flask(__name__)
@@ -13,6 +12,18 @@ def hello_world():
 
 @app.route("/quiz/<username>")
 def show_quiz(username=None):
-    returnstring, antwort, question_number, progress = quiz.get_question(quiz, username)
-    return render_template('quiz.html', string = returnstring)
+    # returnstring, antwort, question_number, progress = quiz.get_question(quiz, username)
+    daten = quiz.get_question(quiz, username)
+    # print(daten.q_question)
+    return render_template('quiz.html', daten = daten, username=username)
+
+@app.route("/answer/<selection>/<username>/<questionnumber>")
+def answer(selection=None,username=None,questionnumber=None):
+    quiz.antwort(quiz,question_number=questionnumber, username=username,korrekt=selection)
+    return redirect(f"/continue/{username}")
+
+@app.route("/continue/<username>")
+def cont(username=None):
+    return render_template('continue.html', username=username)
+
 
--- WebApp/selbst_kontrolle_script.py
+++ WebApp/selbst_kontrolle_script.py
@@ -67,11 +67,14 @@ class Quiz:
                                                     'question': f"Wenn du ein Fall bearbeitest, in dem das Gesetzt \"{row['Artikel']}. {row['Titel']}\" zu Grunde liegt, welche Gesetze lohnt es sich nochmal anzuschauen?",
                                                     'answer': row['Verweißende Gesetze']}
 
+        for key, value in self.laws_question.items():
+            self.all_possible_question[len(self.all_possible_question)] = value
+
     def get_question(self, username):
         # StartUp of Program:
         progress = None
         try:
-            progress = joblib.load(f'./progress{1}.pkl'(username))
+            progress = joblib.load(f'./progress/progress{username}.pkl')
             temp_diff = (progress.endtime - progress.starttime)
             progress.starttime = datetime.now() - temp_diff
             progress.endtime = datetime.now()
@@ -81,42 +84,70 @@ class Quiz:
         except Exception as e:
             progress = Progress()
             # i = input(f'Möchtest du auch die Gesetze lernen? JA (j)')
-            i = 'j'
-            if i == 'j':
-                for key, value in self.laws_question.items():
-                    self.all_possible_question[len(self.all_possible_question)] = value
+            # i = 'j'
+            # if i == 'j':
+            #     for key, value in self.laws_question.items():
+            #         self.all_possible_question[len(self.all_possible_question)] = value
 
         # while len(progress.succesfull) != len(progress.all_possible_question):
             # os.system('cls')
         timediff = (len(self.all_possible_question) - len(progress.succesfull)) * (datetime.now() - progress.starttime)
         timediff = str(timediff).split(".")[0]
-        returnstring = (f'Statusbericht:\t\t'
-                f'\nAnzahl an gegeben Antworten: {progress.answercounter}\t\t So viele Fragen gibt es aktuell: {len(self.all_possible_question)}'
-                f'\nSo viel kannst du schon: {round(100/len(self.all_possible_question)*len(progress.succesfull), 2)}%'
-                f'\nZeit bis du wahrscheinlich alles kannst: {timediff}'
-                f'\n-----------------------------------------------------')
+        # returnstring = (f'Statusbericht:\t\t'
+        #         f'\nAnzahl an gegeben Antworten: {progress.answercounter}\t\t So viele Fragen gibt es aktuell: {len(self.all_possible_question)}'
+        #         f'\nSo viel kannst du schon: {round(100/len(self.all_possible_question)*len(progress.succesfull), 2)}%'
+        #         f'\nZeit bis du wahrscheinlich alles kannst: {timediff}'
+        #         f'\n-----------------------------------------------------')
 
 
         question_number = random.randint(0, len(self.all_possible_question))
         while question_number in progress.succesfull:
             question_number = random.randint(0, len(self.all_possible_question))
 
-        returnstring += f"{self.all_possible_question[question_number]['section']}\n"
-        returnstring += f"\033[94m{self.all_possible_question[question_number]['question']}\033[0m"
-        returnstring +=(f'\n -> Antwort anzeigen (Enter drücken)')
-        antwort = (f"\n\033[94m{self.all_possible_question[question_number]['answer']}\033[0m")
+        # returnstring += f"{self.all_possible_question[question_number]['section']}\n"
+        # returnstring += f"\033[94m{self.all_possible_question[question_number]['question']}\033[0m"
+        # returnstring +=(f'\n -> Antwort anzeigen (Enter drücken)')
+        # antwort = (f"\n\033[94m{self.all_possible_question[question_number]['answer']}\033[0m")
         # i = input(f'\n -> Korrekt geantwortet ? JA (j)')
         # if i == 'j':
-        return returnstring, antwort, question_number, progress
 
-    def antwort(self, question_number, korrekt, progress, username):
-            if korrekt > 0:
-                progress.succesfull.append(question_number)
+        daten = DataObj()
+        daten.answercounter = progress.answercounter
+        daten.num_all_possible_question = len(self.all_possible_question)
+        daten.num_succesfull = round(100/len(self.all_possible_question)*len(progress.succesfull), 2)
+        daten.timediff = timediff
+        daten.q_section = self.all_possible_question[question_number]['section'].replace("\n"," ")
+        daten.q_question = self.all_possible_question[question_number]['question'].replace("\n"," ")
+        daten.q_answer = self.all_possible_question[question_number]['answer'].replace("\n"," ")
+        daten.q_number = question_number
 
-            progress.answercounter += 1
-            progress.endtime = datetime.now()
-            joblib.dump(progress, f'./progress{1}.pkl'(username))
+        return daten
 
-            return len(progress.succesfull) != len(progress.all_possible_question)
+    def antwort(self, question_number, korrekt, username):
+        progress = None
+        try:
+            progress = joblib.load(f'./progress/progress{username}.pkl')
+        except Exception as e:
+            progress = Progress()
+
+        if korrekt == "1":
+            progress.succesfull.append(question_number)
+
+        progress.answercounter += 1
+        progress.endtime = datetime.now()
+        joblib.dump(progress, f'./progress/progress{username}.pkl')
+
+        # return len(progress.succesfull) != len(progress.all_possible_question)
 
     # input('DU HAST ALLE FRAGEN GESCHAFFT !')
+
+class DataObj:
+    def __init__(self):
+        self.answercounter = 0
+        self.num_all_possible_question = 0
+        self.num_succesfull = 0
+        self.timediff = 0
+        self.q_section = ""
+        self.q_question = ""
+        self.q_answer = ""
+        self.q_number = 0
